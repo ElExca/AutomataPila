7597d8564363bff0b6e1894519b0c5d7204ccc29
ElExca <emili17_2002@hotmail.com>
2024-01-23 22:29:22 -0600

    LogicaLexemas añadida

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,84 @@
 import React, { useState } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 
+//Definicion de tokens y lexemas
+const tokenTypes = [
+ 
+  { regex: /^:/, token: "DosPuntos" },
+  { regex: /^_/, token: "GuionBajo" },
+  { regex: /^{/, token: "LlaveApertura" },
+  { regex: /^}/, token: "LlaveFinal" },
+  { regex: /^\[/, token: "CorcheteApertura" },
+  { regex: /^\]/, token: "CorcheteCierre" },
+  { regex: /^\(/, token: "ParentesisApertura" },
+  { regex: /^\)/, token: "ParentesisCierre" },
+  { regex: /^\d+/, token: "Digitos" },
+  { regex: /^FCN/, token: "PalabraReservadaFuncion" },
+  { regex: /^for/, token: "PalabraReservadaFor" },
+  { regex: /^if/, token: "PalabraReservadaIf" },
+  { regex: /^\+\+/, token: "Incremento" },
+  { regex: /^--/, token: "Decremento" },
+  { regex: /^>/, token: "OperadorMayor" },
+  { regex: /^</, token: "OperadorMenor" },
+  { regex: /^>=/, token: "OperadorIgualMayor" },
+  { regex: /^<=/, token: "OperadorIgualMenor" },
+  { regex: /^===/, token: "OperadorIgualdad" },
+  { regex: /^[a-zA-Z]+/, token: "Letras" },
+];
+//logica para la identificación de los lexemas
+function lex(input) {
+  let tokens = [];
+  let position = 0;  // Seguimiento de la posición para mensajes de error más informativos.
+
+  while (input.length > 0) {
+    // Ignora espacios y saltos de línea al principio de la cadena de entrada.
+    const whitespace = input.match(/^\s+/);
+    if (whitespace) {
+      position += whitespace[0].length;  // Actualiza la posición.
+      input = input.slice(whitespace[0].length);  // Elimina los espacios de la entrada.
+    }
+
+    if(input.length === 0) {
+      break;  // Si solo quedan espacios en blanco, termina el bucle.
+    }
+
+    let match = false;
+    for (let tokenType of tokenTypes) {
+      const result = tokenType.regex.exec(input);
+      if (result !== null) {
+        match = true;
+        tokens.push({ type: tokenType.token, value: result[0] });
+        position += result[0].length;  // Actualiza la posición.
+        input = input.slice(result[0].length);
+        break;
+      }
+    }
+    if (!match) {
+      const errorToken = input[0];
+      tokens.push({ type: "Error", value: `Carácter inesperado '${errorToken}' en la posición ${position}.` });
+      break;
+    }
+  }
+  return tokens;
+}
+
+
+
 function App() {
   const [cadena, setCadena] = useState('');
   const [resultado, setResultado] = useState(null);
   const [pilaInfo, setPilaInfo] = useState([]);
+  const [tokens, setTokens] = useState([]);
 
   const analizarCadena = () => {
+    const tokensGenerados = lex(cadena);
+    setTokens(tokensGenerados);
     const { esValida, infoPila } = analizadorSintactico(cadena);
     setResultado(esValida);
     setPilaInfo(infoPila);
   };
 
   const editorDidMount = (editor, monaco) => {
-    // Puedes realizar acciones cuando el editor se monta
     console.log('Editor montado');
   };
 
@@ -30,18 +95,18 @@ function App() {
           editorDidMount={editorDidMount}
         />
         <div style={{ marginLeft: '20px' }}>
-          <h3>Información de Pila:</h3>
-          <ul>
-            {pilaInfo.map((info, index) => (
-              <li key={index}>{info}</li>
-            ))}
-          </ul>
+        <h3>Tokens Generados:</h3>
+        <ul>
+          {tokens.map((token, index) => (
+            <li key={index}>{`${token.type} (${token.value})`}</li>
+          ))}
+        </ul>
         </div>
       </div>
       <button onClick={analizarCadena}>Analizar</button>
-      {resultado !== null && (
+      {/* {resultado !== null && (
         <p>La cadena {cadena} es {resultado ? 'válida' : 'inválida'}.</p>
-      )}
+      )} */}
     </div>
   );
 }
